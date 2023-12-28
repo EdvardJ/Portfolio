9e4e414853ee286c59c815e62208cbd60b344501
EdvardJ <eetu.jacobson@gmail.com>
2023-12-28 20:16:57 +0200

    commit

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -12,7 +12,7 @@ export default function Skills() {
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Technologies and skills I have studied in school and used in my projects
+            Technologies and skills I have studied in school and used in my projects so far
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
